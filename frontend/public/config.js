53dd7234c71bd948841a8ac51f3e242bec62ba3f
Your Name <wickramasooriyabgpv.22@uom.lk>
2026-01-28 15:58:29 +0530

    Update apiBaseUrl in config.js to point to production endpoint

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -3,5 +3,5 @@ window.configs = {
   baseUrl: "https://api.asgardeo.io/t/puleena",
   signInRedirectURL: "https://blogy.choreoapps.dev",
   signOutRedirectURL: "https://blogy.choreoapps.dev",
-  apiBaseUrl: "http://localhost:9090/api",
+  apiBaseUrl: "https://e3e9a2a8-dd59-404d-b310-652b77ccaa7c-prod.e1-us-east-azure.choreoapis.dev/default/blogy-be/v1.0",
 };
